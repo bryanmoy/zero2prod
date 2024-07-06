e163eac9367aae0a7f5860c40cae07415229147d
Bryan Moy <dev@bryanmoy.com>
2024-07-06 22:28:43 +0200

    chore(devenv): Add devenv

---
+++ devenv.nix
@@ -0,0 +1,39 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.openssl ];
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+  # enterShell = ''
+  #   hello
+  #   git --version
+  # '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    cargo test
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+  services.postgres = {
+    enable = true;
+  };
+
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
